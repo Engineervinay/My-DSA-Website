00b92ef915726a55fbdee9b4f35502b1448eb4d5
Vinay Patil <vinay1645.vp@gmail.com>
2023-03-22 12:39:48 +0530

    UI improvements

--- src/components/SubAccordion.js
+++ src/components/SubAccordion.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CopyBlock, dracula } from "react-code-blocks";
 
-import {AiFillFileText,AiOutlineMinus,AiOutlinePlus,AiOutlineYoutube} from "react-icons/ai"
+import {AiFillFileText,AiOutlineMinus,AiOutlinePlus} from "react-icons/ai"
 import{SiYoutube,SiGeeksforgeeks,SiLeetcode} from "react-icons/si"
 import '../App.css';
 import { Collapse } from 'react-collapse';
@@ -10,7 +10,7 @@ function SubAccordion({open,toggle,name,gfg,leetcode,article,youtube,code}) {
   // and the background 
   return (
     <div className='pt-[10px]'>
-      <div className="bg-white bg-opacity-10 rounded-t-lg py-[25px] px-[50px] flex justify-between items-center cursor-pointer" onClick={toggle}>
+      <div className="bg-cyan-400 bg-opacity-10 rounded-t-lg py-[25px] px-[50px] flex justify-between items-center cursor-pointer" onClick={toggle}>
         <p className="text-[22px] font-semibold">{name}</p>
         <div className='text-[27px] flex space-x-3 '>
         <a href={youtube}target="_blank" rel='noreferrer'> <SiYoutube/></a>
@@ -23,7 +23,7 @@ function SubAccordion({open,toggle,name,gfg,leetcode,article,youtube,code}) {
       </div>
       <Collapse isOpened={open}>
 
-        <div className='bg-white bg-opacity-10 rounded-b-lg px-[50px] pb-[20px] '>
+        <div className='bg-cyan-400 bg-opacity-10 rounded-b-lg px-[50px] pb-[20px] '>
         <CopyBlock
     text={code}
     language={"cpp"}
